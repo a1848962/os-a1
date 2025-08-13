6a319f9286d26fdc210cb0b2489676a637f4cf9e
tobygrice <tobygrice@icloud>
2025-08-13 14:04:59 +0930

    even.c complete

--- even.c
+++ even.c
@@ -1,13 +1,24 @@
-#include <stdio.h> // io
-#include <stdlib.h> // malloc
-#include <unistd.h> // sleep
-#include <signal.h> // signals
+#include <signal.h>  // signals
+#include <stdio.h>   // io
+#include <unistd.h>  // sleep
+
+void handleSIGHUP(int sig) { printf("Ouch!"); }
+
+void handleSIGINT(int sig) { printf("Yeah!"); }
 
 int main(int argc, char** argv) {
-  int even_nums_to_print;
-  scanf("%d", &even_nums_to_print);
+  signal(SIGHUP, handleSIGHUP);
+  signal(SIGINT, handleSIGINT);
 
-  for (int i=0; i<even_nums_to_print; i++) {
-    printf("%d ", i*2);
+  if (argc != 2) {
+    printf("Enter 1 argument");
+    exit(1);
+  }
+
+  int even_nums_to_print = atoi(argv[1]);
+
+  for (int i = 0; i < even_nums_to_print; i++) {
+    printf("%d\n", i * 2);
+    sleep(5);
   }
 }
